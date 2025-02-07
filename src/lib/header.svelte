ca89fe248d528cec92f244dc085e63dc7d9f2cce
panbread <93918332+Panniku@users.noreply.github.com>
2025-02-07 22:11:13 +0400

    add header (dummy commit)

---
+++ src/lib/header.svelte
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/">1</a>
+    <a href="/">2</a>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import Header from "$lib/header.svelte";
+</script>
+
+<Header></Header>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+
+</style>
