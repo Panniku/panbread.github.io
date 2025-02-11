a30dd0a615228f8c40a4cd5147ddd1ee1268ffff
panbread <93918332+Panniku@users.noreply.github.com>
2025-02-11 14:54:31 +0400

    Updates

---
+++ _app/immutable/chunks/D-ul8MlS.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_18403eg)==null?void 0:s.base)??"/panniku.github.io";var e;const t=((e=globalThis.__sveltekit_18403eg)==null?void 0:e.assets)??a;export{t as a,a as b};
