997811060735974b20e3db74c0ccd8cd54d5732c
panbread <93918332+Panniku@users.noreply.github.com>
2025-02-16 21:25:05 +0400

    Updates

---
+++ _app/immutable/chunks/BqjP11tv.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_11qvi5a)==null?void 0:s.base)??"/panniku.github.io";var a;const e=((a=globalThis.__sveltekit_11qvi5a)==null?void 0:a.assets)??t;export{e as a,t as b};
